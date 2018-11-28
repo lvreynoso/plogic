c482a42c2ab87e3b7b39f408f90dbe54c7477828
Lucia Reynoso <lvreynoso@protonmail.com>
2018-11-27 22:45:20 -0800

    added connectors and connector focii

--- public/scripts/devices.js
+++ public/scripts/devices.js
@@ -10,6 +10,21 @@ class AndGate {
         this.x = x;
         this.y = y;
         this.held = false;
+        this.inputOne = {
+            x: undefined,
+            y: undefined,
+            power: false
+        }
+        this.inputTwo = {
+            x: undefined,
+            y: undefined,
+            power: false
+        }
+        this.output = {
+            x: undefined,
+            y: undefined,
+            power: false
+        }
     }
 
     hold() {
@@ -32,6 +47,10 @@ class AndGate {
         ctx.lineTo(this.x + 50, this.y);
         ctx.closePath();
         ctx.stroke();
+
+        this.drawInputOne(ctx);
+        this.drawInputTwo(ctx);
+        this.drawOutput(ctx);
     }
 
     drawFocus(ctx) {
@@ -42,7 +61,50 @@ class AndGate {
         ctx.strokeStyle = 'black';
     }
 
+    drawInputOne(ctx) {
+        // draw a connecting hook on the top left wire of the AND gate
+        ctx.beginPath();
+        ctx.arc(this.x - 75, this.y - 30, 5, (3/2) * Math.PI, (5/2) * Math.PI, false);
+        ctx.stroke();
+
+        this.inputOne.x = this.x - 75;
+        this.inputOne.y = this.y - 30;
+    }
+
+    drawInputTwo(ctx) {
+        // draw a connecting hook on the bottom left wire of the AND gate
+        ctx.beginPath();
+        ctx.arc(this.x - 75, this.y + 30, 5, (3/2) * Math.PI, (5/2) * Math.PI, false);
+        ctx.stroke();
+
+        this.inputTwo.x = this.x - 75;
+        this.inputTwo.y = this.y + 30;
+    }
+
+    drawOutput(ctx) {
+        // draw a connecting hook on the right wire of the AND gate
+        ctx.beginPath();
+        ctx.arc(this.x + 55, this.y, 5, (1/2) * Math.PI, (3/2) * Math.PI, false);
+        ctx.stroke();
+
+        this.output.x = this.x + 55;
+        this.output.y = this.y;
+    }
+
+    drawConnectorFocus(ctx, connector) {
+        if (connector != undefined) {
+            ctx.beginPath();
+            ctx.arc(connector.x, connector.y, 10, 0, 2 * Math.PI, true)
+            ctx.strokeStyle = 'purple';
+            ctx.stroke();
+            ctx.strokeStyle = 'black';
+        }
+    }
+
     update(ctx, mouse) {
+        // update power
+        this.output.power = this.inputOne.power && this.inputTwo.power;
+
         // check if the mouse is near us
         let mouseOver = false;
         if (Math.abs(mouse.x - this.x) < 30 && Math.abs(mouse.y - this.y) < 30) {
@@ -66,6 +128,19 @@ class AndGate {
             }
         }
 
+        // check if the mouse is near a connector
+        let mouseNearConnector = undefined;
+        let connectors = [this.inputOne, this.inputTwo, this.output]
+        for (let i = 0; i < connectors.length; i++) {
+            if (Math.abs(mouse.x - connectors[i].x) < 10 && Math.abs(mouse.y - connectors[i].y) < 10) {
+                mouseNearConnector = connectors[i];
+            }
+        }
+
+        if (mouseNearConnector != undefined) {
+            this.drawConnectorFocus(ctx, mouseNearConnector);
+        }
+
         this.draw(ctx);
         this.draw(ctx);
     }
@@ -82,6 +157,11 @@ class Switch {
         // on/off; off by default
         this.state = false;
         this.held = false;
+        this.connector = {
+            x: this.x,
+            y: this.y + 40,
+            power: this.state
+        }
     }
 
     hold() {
@@ -110,6 +190,7 @@ class Switch {
             ctx.fill();
         }
         ctx.closePath();
+        this.drawConnector(ctx);
         ctx.fillStyle = 'black';
     }
 
@@ -121,7 +202,33 @@ class Switch {
         ctx.strokeStyle = 'black';
     }
 
+    drawConnector(ctx) {
+        // draw a switch connecting hook
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y + 25);
+        ctx.lineTo(this.x, this.y + 30);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y + 35, 5, (3/3) * Math.PI, (6/3) * Math.PI, false);
+        ctx.stroke();
+
+        this.connector.x = this.x;
+        this.connector.y = this.y + 35;
+    }
+
+    drawConnectorFocus(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.connector.x, this.connector.y, 10, 0, 2 * Math.PI, true)
+        ctx.strokeStyle = 'purple';
+        ctx.stroke();
+        ctx.strokeStyle = 'black';
+    }
+
     update(ctx, mouse) {
+        // update connector power
+        this.connector.power = this.state;
+
         // check if the mouse is near us
         let mouseOver = false;
         if (Math.abs(mouse.x - this.x) < 30 && Math.abs(mouse.y - this.y) < 30) {
@@ -156,6 +263,16 @@ class Switch {
             }
         }
 
+        // check if the mouse is near a connector
+        let mouseNearConnector = false;
+        if (Math.abs(mouse.x - this.connector.x) < 10 && Math.abs(mouse.y - this.connector.y) < 10) {
+            mouseNearConnector = true;
+        }
+
+        if (mouseNearConnector == true) {
+            this.drawConnectorFocus(ctx);
+        }
+
         this.draw(ctx);
         this.draw(ctx);
     }
@@ -172,6 +289,11 @@ class Bulb {
         // on/off; off by default
         this.state = false;
         this.held = false;
+        this.connector = {
+            x: this.x,
+            y: this.y + 40,
+            power: false
+        }
     }
 
     draw(ctx) {
@@ -185,6 +307,7 @@ class Bulb {
             ctx.fillStyle = 'yellow';
             ctx.fill();
         }
+        this.drawConnector(ctx);
         ctx.fillStyle = 'black';
     }
 
@@ -196,7 +319,33 @@ class Bulb {
         ctx.strokeStyle = 'black';
     }
 
+    drawConnector(ctx) {
+        // draw a lightbulb connecting hook
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y + 30);
+        ctx.lineTo(this.x, this.y + 35);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y + 40, 5, (3/3) * Math.PI, (6/3) * Math.PI, false);
+        ctx.stroke();
+
+        this.connector.x = this.x;
+        this.connector.y = this.y + 40;
+    }
+
+    drawConnectorFocus(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.connector.x, this.connector.y, 10, 0, 2 * Math.PI, true)
+        ctx.strokeStyle = 'purple';
+        ctx.stroke();
+        ctx.strokeStyle = 'black';
+    }
+
     update(ctx, mouse) {
+        // update power
+        this.state = this.connector.power
+
         // check if the mouse is near us
         let mouseOver = false;
         if (Math.abs(mouse.x - this.x) < 30 && Math.abs(mouse.y - this.y) < 30) {
@@ -219,6 +368,16 @@ class Bulb {
                 // if not being held, draw a focus ring
                 this.drawFocus(ctx);
             }
+        }
+
+        // check if the mouse is near a connector
+        let mouseNearConnector = false;
+        if (Math.abs(mouse.x - this.connector.x) < 10 && Math.abs(mouse.y - this.connector.y) < 10) {
+            mouseNearConnector = true;
+        }
+
+        if (mouseNearConnector == true) {
+            this.drawConnectorFocus(ctx);
         }
 
         this.draw(ctx);
